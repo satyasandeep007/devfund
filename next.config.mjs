d511b823de112ffd22caaa0e0ebdd9942c09b523
Satya Sandeep <satyasandeep786@gmail.com>
2024-10-13 02:39:07 +0530

    feat: change dashboard layout

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["icoholder.com", "assets.aceternity.com"],
+    domains: [
+      "icoholder.com",
+      "assets.aceternity.com",
+      "avatars.githubusercontent.com",
+    ],
   },
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding", {
--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,71 +1,144 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { Sidebar, SidebarBody, SidebarLink } from "@/components/Sidebar";
-import { IconArrowLeft, IconBrandTabler } from "@tabler/icons-react";
-import { useAccount, useDisconnect } from "wagmi";
-import DashboardHeader from "@/components/Header";
-import { useRouter } from "next/navigation"; // Updated import for client-side navigation
+import Image from "next/image";
+import Link, { LinkProps } from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import {
+  IconLogout,
+  IconSettings,
+  IconUser,
+  IconGitBranch,
+  IconBrandGithub,
+  IconFiles,
+  IconSpeakerphone,
+} from "@tabler/icons-react";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const [open, setOpen] = useState(false);
-  const { isConnected } = useAccount();
-  const { disconnect } = useDisconnect();
-  const router = useRouter();
-
-  // useEffect(() => {
-  //   if (!isConnected) {
-  //     router.push(`/`);
-  //   }
-  // }, [isConnected, router]);
-
-  const handleDisconnect = () => {
-    disconnect();
-    router.push(`/`);
-  };
-
-  const links = [
-    {
-      label: "Dashboard",
-      href: "/user/dashboard",
-      icon: (
-        <IconBrandTabler className="text-[#222222] dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-      ),
-    },
-
-    {
-      label: "Logout",
-      href: "#",
-      onClick: () => handleDisconnect(),
-      icon: (
-        <IconArrowLeft className="text-[#222222] dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-      ),
-    },
-  ];
+function Sidebar() {
+  const pathname = usePathname();
 
   return (
-    <div className="flex h-screen bg-[#FCFAF6] dark:bg-gray-900">
-      <Sidebar open={open} setOpen={setOpen}>
-        <SidebarBody className="justify-between gap-10">
+    <div className="w-60 shrink-0 md:block h-screen sticky top-0 overflow-hidden">
+      <div className="w-full h-full bg-white border-r">
+        <div className="p-4 md:p-6 flex group items-center gap-2">
+          <div>
+            <h1 className="text-sm font-medium text-gray-800">Git Campaigns</h1>
+            <p className="text-xs text-gray-500 font-small">Base India 2024</p>
+            <IconBrandGithub />
+          </div>
+        </div>
 
-          <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden relative">
-            <img src="/menu.svg"  className="w-10 h-10" />
-            <div className="mt-8 flex flex-col gap-2">
-              {links.map((link, idx) => (
-                <SidebarLink key={idx} link={link} />
-              ))}
+        <hr className="bg-gray-400 mx-2" />
+
+        <div className="flex flex-col h-full justify-between">
+          {/* top */}
+          <div className="pt-6 text-gray-500 font-medium space-y-2 md:px-2 text-xs">
+            <Link
+              href={"/app/repositories"}
+              className={`flex ${
+                pathname === "/app/repositories" ? "text-orange-700" : ""
+              } hover:px-8 duration-200 px-6 py-2 items-center gap-2`}
+            >
+              <IconFiles />
+              Repositories
+            </Link>
+            <Link
+              href={"/app/pull-requests"}
+              className={`flex ${
+                pathname === "/app/pull-requests" ? "text-orange-700" : ""
+              } hover:px-8 duration-200 px-6 py-2 items-center gap-2`}
+            >
+              <IconGitBranch />
+              Pull Requests
+            </Link>
+
+            <Link
+              href={"/app/campaigns"}
+              className={`flex ${
+                pathname === "/app/campaigns" ? "text-orange-700" : ""
+              } hover:px-8 duration-200 px-6 py-2 items-center gap-2`}
+            >
+              <IconSpeakerphone />
+              Campaigns
+            </Link>
+            <Link
+              href={"/app/profile"}
+              className={`flex ${
+                pathname === "/app/profile" ? "text-orange-700" : ""
+              } hover:px-8 duration-200 px-6 py-2 items-center gap-2`}
+            >
+              <IconUser />
+              Profile
+            </Link>
+            <Link
+              href={"/app/settings"}
+              className={`flex ${
+                pathname === "/app/settings" ? "text-orange-700" : ""
+              } hover:px-8 duration-200 px-6 py-2 items-center gap-2`}
+            >
+              <IconSettings />
+              Settings
+            </Link>
+          </div>
+          <div>
+            <div className="text-gray-500 text-xs font-medium md:px-2">
+              <button
+                className={`flex ${
+                  pathname === "/app/settings" ? "text-primary" : ""
+                } hover:px-8 duration-200 px-6 py-2 items-center gap-2`}
+              >
+                <IconLogout size={16} />
+                <a
+                  className="text-gray-500"
+                  onClick={(e) => {
+                    e.preventDefault();
+                  }}
+                >
+                  Logout
+                </a>
+              </button>
+            </div>
+
+            <hr className="bg-gray-400 mx-2 my-4" />
+
+            {/* bottom */}
+            <div className="flex pb-32 justify-between px-4 md:px-6 items-center cursor-pointer hover:pr-5 duration-200">
+              <div className="flex items-center gap-2">
+                <Image
+                  src={"https://avatars.githubusercontent.com/u/52450973?v=4"}
+                  alt={"user"}
+                  width={36}
+                  height={36}
+                  className="rounded-full"
+                />
+
+                <div className="">
+                  <p className="text-sm font-semibold text-gray-800">
+                    John Doe
+                  </p>
+                  <p className="text-xs font-medium text-gray-500">@johndoe</p>
+                </div>
+              </div>
             </div>
           </div>
-        </SidebarBody>
-      </Sidebar>
-      <div className="flex-1 ml-40 flex flex-col">
-        <DashboardHeader />
-        <main className="flex-1 p-6 overflow-auto">{children}</main>
+        </div>
       </div>
     </div>
   );
 }
+
+const NavLink = React.forwardRef<
+  LinkProps,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, href, ...props }) => (
+  <Link
+    href={href!}
+    className={`flex ${
+      window.location.pathname === href! ? "text-primary" : ""
+    } hover:px-8 duration-200 rounded-md w-full py-2 px-6 items-center gap-2`}
+    {...props}
+  />
+));
+NavLink.displayName = "NavLink";
+
+export default Sidebar;
