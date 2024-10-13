0ffebc17c9f96d2cb631a962af38827c7f655fb8
vinay4656 <vinayvinnu2646@gmail.com>
2024-10-13 14:38:31 +0530

    new hero

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { headers } from "next/headers";
-
-import { cookieToInitialState } from "wagmi";
-
-import { config } from "@/lib/wagmiConfig";
 import AppKitProvider from "@/app/providers";
 import { Loading } from "@/components/Loading";
+import Header from "@/components/header";
 
 export const metadata: Metadata = {
-  title: "Base India",
-  description: "Base India",
+  title: "RefineCode",
+  description: "open source, cross-platform refine your code",
 };
 
 export default function RootLayout({
@@ -18,21 +14,17 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const initialState = cookieToInitialState(config, headers().get("cookie"));
   return (
     <html lang="en">
-      <head>
-        <link
-          href="https://api.fontshare.com/v2/css?f[]=epilogue@100,101,200,201,300,400,401,500,501,600,601,700,701,800&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body>
-        <Loading>
-          <AppKitProvider initialState={initialState}>
-            {children}
-          </AppKitProvider>
-        </Loading>
+      <body className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Loading>
+            <AppKitProvider>
+              {children}
+            </AppKitProvider>
+          </Loading>
+        </main>
       </body>
     </html>
   );
