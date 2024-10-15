542a8b80757ebba7d9262c972eb63853d9946cb4
Satya Sandeep <satyasandeep786@gmail.com>
2024-10-15 21:49:14 +0530

    feat: Add recieve modal

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,13 +12,22 @@ const Header = ({ isConnected }: any) => {
               width={40}
               height={40}
               className="mr-2"
+              alt="logo"
             />
             <a href="/" className="text-black hover:text-gray-700">
               DevFund
             </a>
           </h1>
         </div>
-       
+
+        <div className="w-full">
+          {" "}
+          <nav aria-label="Main navigation w-full">
+            <ul className="flex items-center justify-center space-x-6">
+              <li>{/*  */}</li>
+            </ul>
+          </nav>
+        </div>
         <div className="w-full flex justify-end">
           <div className="flex justify-center items-center">
             {!isConnected ? (
