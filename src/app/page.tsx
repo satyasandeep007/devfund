c43936bc01953290f2223d01bd4f35bf2073abaa
Satya Sandeep <satyasandeep786@gmail.com>
2024-10-15 13:00:24 +0530

    feat: update header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,58 +2,27 @@
 
 import { useAccount } from "wagmi";
 import Hero from "@/components/Hero";
-import { useRouter } from "next/navigation"; // Updated import for client-side navigation
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-
 import { useSession, signIn } from "next-auth/react";
+
 import Header from "@/components/header";
+
 const Home: React.FC = () => {
   const { isConnected } = useAccount();
-  const router = useRouter(); // Use useRouter for client-side navigation
-  const { status, data: session }: any = useSession();
 
-  // useEffect(() => {
-  //   if (isConnected) {
-  //     router.push(`/user/dashboard`); // Use router.push for navigation
-  //   }
-  // }, [isConnected, router]);
+  const router = useRouter(); // Use useRouter for client-side navigation
+  const { data: session }: any = useSession();
+
+  useEffect(() => {
+    if (isConnected) {
+      router.push(`/${session.user.username}/dashboard`); // Use router.push for navigation
+    }
+  }, [isConnected, router, session.user.username]);
 
   return (
     <main className="h-screen bg-[#f0f0f0] dark:bg-[#1a1a1a]">
-      {/* <div className="w-full max-w-7xl mx-auto py-8 flex justify-between items-center">
-        <p className="text-[26px] font-bold text-left uppercase">
-          <span className="text-[#222] dark:text-white">Base India</span> {""}
-        </p>
-
-        <div className="flex gap-2">
-          <div className="flex justify-center items-center">
-            {!isConnected ? (
-              <w3m-connect-button size="sm" />
-            ) : (
-              <>
-                <w3m-network-button />
-                <w3m-account-button balance={"show"} />
-              </>
-            )}
-          </div>
-          <button
-            onClick={() => {
-              if (status === "authenticated") {
-                router.push(`/${session?.user?.username}/dashboard`);
-              } else {
-                signIn();
-              }
-            }}
-            className="button hidden rounded-[50px] border-[#7F8995] bg-transparent text-black after:bg-colorOrangyRed hover:border-colorOrangyRed hover:text-blue-400 lg:inline-block"
-          >
-            {status === "authenticated"
-              ? `Logged in as ${session?.user?.username}`
-              : "Login"}
-            →
-          </button>
-        </div>
-      </div> */}
-      <Header />
+      <Header isConnected={isConnected} />
       <Hero />
     </main>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 
-const Header: React.FC = () => {
+const Header = ({ isConnected }: any) => {
   return (
     <header className="bg-[#f0f0f0] p-4">
       <div className="flex justify-between items-center max-w-7xl mx-auto">
         <div className="flex items-center w-full">
           <h1 className="text-2xl font-bold mr-8">
-            <a href="/" className="text-black hover:text-gray-700">DevFund®</a>
+            <a href="/" className="text-black hover:text-gray-700">
+              DevFund®
+            </a>
           </h1>
-         
         </div>
-       <div className="w-full"> <nav aria-label="Main navigation w-full">
+        <div className="w-full">
+          {" "}
+          <nav aria-label="Main navigation w-full">
             <ul className="flex items-center justify-center space-x-6">
               <li>
                 {/* <a href="/about" className=" hover:text-blue-700">
@@ -28,14 +31,19 @@ const Header: React.FC = () => {
                 </a> */}
               </li>
             </ul>
-          </nav></div>
+          </nav>
+        </div>
         <div className="w-full flex justify-end">
-          <a 
-            href="/download" 
-            className="bg-black text-white px-4 py-2 hover:bg-blue-700"
-          >
-            Connect Wallet
-          </a>
+          <div className="flex justify-center items-center">
+            {!isConnected ? (
+              <w3m-connect-button size="sm" />
+            ) : (
+              <>
+                <w3m-network-button />
+                <w3m-account-button balance={"show"} />
+              </>
+            )}
+          </div>
         </div>
       </div>
     </header>
