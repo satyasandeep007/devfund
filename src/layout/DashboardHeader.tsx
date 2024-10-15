abfcf7760c358d49434c94452db6a3c290b42576
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2024-10-15 17:06:19 +0530

    fix: Fix Dashboard Header

--- src/layout/DashboardHeader.tsx
+++ src/layout/DashboardHeader.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useAccount } from "wagmi";
-// import ThemeToggle from "@/components/ThemeToggle";
 
 const DashboardHeader: React.FC = () => {
   const { isConnected } = useAccount();
@@ -10,18 +9,17 @@ const DashboardHeader: React.FC = () => {
     <header className="w-full py-4 px-8 dark:bg-gray-900 flex justify-between items-center ">
       <div className="flex items-center"></div>
       <div className="flex items-center gap-x-4">
-        {/* <ThemeToggle /> */}
-        <div className="flex items-center gap-x-3">
-          {!isConnected ? (
-            <w3m-connect-button />
-          ) : (
-            <>
-            <div className="flex items-center gap-x-3">
-              <w3m-network-button />
-              <w3m-account-button balance={"show"} />
-            </div>
-            </>
-          )}
+        <div className="w-full flex justify-end">
+          <div className="flex justify-center items-center">
+            {!isConnected ? (
+              <w3m-connect-button size="sm" />
+            ) : (
+              <div className="flex items-center gap-2">
+                <w3m-network-button />
+                <w3m-account-button balance={"show"} />
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </header>
